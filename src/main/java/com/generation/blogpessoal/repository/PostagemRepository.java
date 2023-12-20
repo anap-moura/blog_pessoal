21533a0afc3f95ccc8e34f13595acad2de6b565e
Ana Paula Moura <ana.mcruz@hotmail.com>
2023-12-20 18:27:42 -0300

    PostagemController com todos os metodos

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,12 +1,18 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	
+	public List <Postagem> findAllByTituloContainingIgnoreCase (@Param("titulo") String titulo);
 }
+
+
+
 
 /*Uma interface em Java é uma Classe Abstrata (uma Classe que serve de modelo para outras Classes),
 composta somente por Métodos abstratos. E como tal, obviamente não pode ser instanciada, ou seja, 
